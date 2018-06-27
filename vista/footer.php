0ea745c52eb3d36492ac9b712644fabae5b37270
luis <ldiazpernia@gmail.com>
2018-06-27 15:40:48 -0400

    Footer

--- vista/footer.php
+++ vista/footer.php
@@ -49,11 +49,11 @@
 									</li>
 									<li>
 										<i class="fa fa-globe"></i>
-										<span><a href="mailto:info@roofadvisorz.com ">info@roofadvisorz.com</a> </span>
+										<span><a href="mailto:info@roofadvisorz.com">info@roofadvisorz.com</a> </span>
 									</li>
 									<li>
 										<i class="fa fa-globe"></i>
-										<span> Web : <a href-'http://www.roofadvisorz.com'>www.roofadvisorz.com</span>
+										<span> Web : <a href='http://www.roofadvisorz.com'>www.roofadvisorz.com</span>
 									</li>
 									<p>
 									<a href='?controller=terms_conditions&accion=showinfo'>Legal</a>
