d0748f885a02ca5a8a3c3e2a31de723d9c923787
luis <ldiazpernia@gmail.com>
2018-07-10 16:00:13 -0400

    fooder
    Signed-off-by: luis <ldiazpernia@gmail.com>

--- vista/termsconditions.php
+++ vista/termsconditions.php
@@ -1,46 +1,121 @@
 
+
 <html class="no-js" lang="en">
+
     
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
 
-        <!-- Add your site or application content here -->
-		
-		<!-- breadcrum-area start-->
-		<div class="breadcrum-area pt-120 pb-100 bg-img-2 bg-opacity">
-			<div class="container">
-				<div class="breadcrum-content">
-					<ol class="breadcrumb">
-					  <li><a href="#">home</a></li>
-					  <li class="active">about us</li>
-					</ol>
-				</div>
+<body>
+
+	<!--[if lt IE 8]>
+
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+
+	<![endif]-->
+
+
+
+	<!-- Add your site or application content here -->
+
+	
+
+	<!-- breadcrum-area start-->
+
+	<div class="breadcrum-area pt-120 pb-100 bg-img-2 bg-opacity">
+
+		<div class="container">
+
+			<div class="breadcrum-content">
+
+				<ol class="breadcrumb">
+
+				  <li><a href="#">home</a></li>
+
+				  <li class="active">about us</li>
+
+				</ol>
+
 			</div>
-		</div>
-		<!-- breadcrum-area end-->
-		<!-- about-area start-->
-		<div class="about-area ptb-80">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12 col-md-12 col-sm-12">
-						<div class="about-text">
-							<h3>WELCOME TO RoofAdvisorZ </h3>
-							<p>Terms and Conditions: Last Updated May 15, 2018</p>
 
-							<h3>Here’s How It Works</h3>
-							<ul>
-								<li>
-                                This Website and the services of RoofAdvisorZ and RoofAdvisorZPro (collectively the "Services") are offered to you, conditioned on your acceptance without modification, of the following terms, conditions, and notices contained in this agreement (the "Terms and Conditions"). Your use of this Website or submission of service requests via telephone constitutes your agreement to all such terms, conditions, and notices in effect at such time. These Terms and Conditions apply to any Services or properties such as websites or apps owned and operated by Viaplix, LLC (including but not limited to RoofAdvisorZ.com, RAZ mobile customer application, RAZ PRO service professional mobile application, the Customer Dashboard website and app, the service professional Dashboard website and app, etc) and collectively referred to herein as "RoofAdvisorZ," on which a link to these Terms & Conditions appears. If you have any questions regarding these Terms & Conditions, you may contact us at www.RoofAdvisorZ.com. We may amend these Terms & Conditions at any time by posting a revised version of these Terms and Conditions. These Terms & Conditions were last updated on February 23th, 2018.
-                                </li>
-							</ul>
-							<a class="btn-more" href="#">learn more</a>
-						</div>
+		</div>
+
+	</div>
+
+	<!-- breadcrum-area end-->
+
+	<!-- about-area start-->
+
+	<div class="about-area ptb-80">
+
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-lg-12 col-md-12 col-sm-12">
+
+					<div class="about-text">
+
+					<!--From Here-->
+
+					<p>Terms and Conditions: Last Updated May 15, 2018</p>
+
+<p>This Website and the services of RoofAdvisorZ and RoofAdvisorZPro (collectively the "Services") are offered to you, conditioned on your acceptance without modification, of the following terms, conditions, and notices contained in this agreement (the "Terms and Conditions"). Your use of this Website or submission of service requests via telephone constitutes your agreement to all such terms, conditions, and notices in effect at such time. These Terms and Conditions apply to any Services or properties such as websites or apps owned and operated by Viaplix, LLC (including but not limited to RoofAdvisorZ.com, RAZ mobile customer application, RAZ PRO service professional mobile application, the Customer Dashboard website and app, the service professional Dashboard website and app, etc) and collectively referred to herein as "RoofAdvisorZ," on which a link to these Terms & Conditions appears. If you have any questions regarding these Terms & Conditions, you may contact us at www.RoofAdvisorZ.com. We may amend these Terms & Conditions at any time by posting a revised version of these Terms and Conditions. These Terms & Conditions were last updated on February 23th, 2018.</p>
+
+
+
+<h5>1. INTRODUCTION TO ROOFADVISORZ</h5>
+
+<p>Upon registration, you may choose your own password or RoofAdvisorZ will assign you a password to access the Services through the RoofAdvisorZ website or mobile apps.</p>
+
+
+
+<h5>2. THE SERVICES</h5>
+
+<p>RoofAdvisorZ is committed to making your total RoofAdvisorZ experience a positive one. There are several ways you can find service professionals through RoofAdvisorZ (collectively, "Service Professionals"). Our Emergency Repair Service allows you to request on demand same day service emergency roof repair via our mobile applications and our web application. Our Schedule Service Repair is our service where you request a service professional for a specific future scheduled day, and where you choose the service professional or where the service professional accepts the scheduled time you requested. You will be able to view all Service Professional’s ratings and reviews.	
+
+</p>
+
+<ul>
+
+<li><b>Emergency Repair Service.</b> Upon entering your information and the request for which you would like a Service Professional, Service Professionals will accept your request for emergency service repair and provide you with an ETA. You will be able to track their service vehicle real time via our mobile application. However, we do not guarantee that a service professional will be able to immediately match your service needs due to high demand in your area or that there are Service Professionals in your area that are either capable or willing to complete your service needs.
+
+</li>
+
+<li><b>Scheduled Repair Services. </b> You can choose to schedule appointments with certain Service Professionals right in the RoofAdvisorZ website or mobile application. Please be aware that RoofAdvisorZ does not guarantee that the Service Professionals will be on time for, or show up for, any such booked appointments, as the Service Professionals are not the employees, contractors or agents of RoofAdvisorZ. RoofAdvisorZ is solely providing the platform to facilitate scheduling of appointments between you and the Service Professionals.	
+
+</li>
+
+<li><b>No Guarantees or Endorsements.</b>Although we take certain steps to examine the credentials of the Service Professionals listed on our website or whom you may book using our Services, we make no guarantees, warranties or representations regarding the skills or undertakings of such Service Professional or the quality of the job that he or she may perform for you if you elect to retain their services. RoofAdvisorZ does not endorse or recommend the services of any particular Service Professional. Except as set forth below in Paragraph 11 and Paragraph 12, we do not independently verify their representations about their services, nor validate any reviews. It is entirely up to you to evaluate the Service Professional and the Service Professional's qualifications, and to enter into a direct contract or otherwise reach agreement with a Service Professional. We do not guarantee or warrant any Service Professional's performance on the job or the outcome or quality of the services performed. The Service Professionals are not employees or agents of RoofAdvisorZ, nor is RoofAdvisorZ an agent of the Service Professionals.</li>
+
+<li><b>No Contracting via the RoofAdvisorZ Website.</b> RoofAdvisorZ may inform you of certain offers or discounts provided by a Service Professional. Such offers or discounts are made solely by the Service Professional, and RoofAdvisorZ does not guarantee or warrant the pricing or discounts that a Service Professional may offer you. Any quotes provided by Service Professionals via the RoofAdvisorZ or apps, or which you find on the RoofAdvisorZ or apps, are not contractually binding offers, are for informational purposes only, and cannot be accepted on or via RoofAdvisorZ. No contractual arrangement is created based upon the quotes provided to you from Service Professionals (or your scheduling of an appointment with a Service Professional) via RoofAdvisorZ. To contract with a Service Professional, you must work directly with the Service Professional. RoofAdvisorZ does not perform, and is not responsible for, any of the Services requested by you in your service request. Your rights under contracts you enter into with Service Professionals are governed by the terms of such contracts and by applicable federal, state, provincial and local laws. RoofAdvisorZ is not a party to such agreements. All payments and applicable taxes must be made to the Service Professional in accordance with the agreements.</li>
+
+<li>
+
+
+
+</li><b>Release from Damages or Claims.</b> Should you have a dispute with respect to any services provided by a Service Professional or the fees charged by any Service Professional, you must address such dispute with the Service Professional directly (although you may copy RoofAdvisorZ on the fee disputes). YOU HEREBY AGREE TO RELEASE RoofAdvisorZ (AND OUR OFFICERS, DIRECTORS, SHAREHOLDERS, AFFILIATES, EMPLOYEES AND AGENTS) FROM ANY DAMAGES OR CLAIMS (INCLUDING CONSEQUENTIAL AND INCIDENTAL DAMAGES) OF EVERY KIND OR NATURE, SUSPECTED AND UNSUSPECTED, KNOWN AND UNKNOWN, AND DISCLOSED OR UNDISCLOSED, ARISING OUT OF OR IN ANY WAY CONNECTED WITH SUCH DISPUTES AND YOUR DEALINGS WITH SERVICE PROFESSIONALS.
+
+</ul>
+
+
+
+
+
+						<a class="btn-more" href="#">learn more</a>
+
+					<!--Close here-->
+
 					</div>
-					
+
 				</div>
+
+				
+
 			</div>
+
 		</div>
-		<!-- about-area end-->
-        
+
+	</div>
+
+	<!-- about-area end-->
+
+	
