0da3150edfd6babdb023d9ad3a82d28fdcfa252d
luis <ldiazpernia@gmail.com>
2018-08-13 23:45:57 -0400

    Signed-off-by: luis <ldiazpernia@gmail.com>

---
+++ controlador/faq1Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+class faq1Controller{
+
+    function __construct()
+	{		
+    }
+    
+    public function showinfo(){
+		require_once("vista/head.php");
+		require_once("vista/faq1.php");
+		require_once("vista/footer.php");
+    }
+    
+}
+?>
