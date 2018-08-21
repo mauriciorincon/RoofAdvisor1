b241272c82365402abc7dc220c089c916750999f
Mauricio Rincon <mauricio.rincon@gmail.com>
2018-08-20 21:53:56 -0500

    mao

--- vista/footer.php
+++ vista/footer.php
@@ -18,7 +18,7 @@
         ?>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
       </div>
     </div>
 
