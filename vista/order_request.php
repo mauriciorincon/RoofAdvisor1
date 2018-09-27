7f387a73daa76f221599d65af3d77cffdfb91a0b
root <root@ded380.hostwindsdns.com>
2018-09-26 20:02:46 -0500

    desing changes revV2

--- vista/order_request.php
+++ vista/order_request.php
@@ -1,8 +1,8 @@
 <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+      <div class="modal-header mheadr">
+        <h3 class="modal-title" id="exampleModalLabel">MEMBER LOGIN</h3>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
