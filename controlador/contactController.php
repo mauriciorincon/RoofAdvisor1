b3c6644456831e24017089b63e0519d436c9a64e
Luis Diaz Pernia <ldiazpernia@Luiss-MacBook-Pro.local>
2018-10-06 15:13:03 -0400

    Envidos del mac

--- controlador/contactController.php
+++ controlador/contactController.php
@@ -7,9 +7,9 @@ class contactController{
     }
     
     public function showinfo(){
-		require_once("vista/head.php");
+	//	require_once("vista/head.php");
 		require_once("vista/contact.php");
-		require_once("vista/footer.php");
+	//	require_once("vista/footer.php");
     }
     
 }
