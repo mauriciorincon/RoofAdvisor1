3758e4ced9c38bae2cba4f5697d3f36cb94178f9
Hernan Mauricio Rincón Bedoya <HMRincon@cajaviviendapopular.gov.co>
2019-01-09 16:39:35 -0500

    update

--- src/controlador/ajax/getValidateAccount.php
+++ src/controlador/ajax/getValidateAccount.php
@@ -3,20 +3,23 @@ if(!isset($_SESSION)) {
     session_start(); 
 } 
 require_once($_SESSION['application_path']."/controlador/userController.php");
-$_account=$_POST['account'];
+if (isset($_POST['account'])){
+    $_account=$_POST['account'];
 
-$_userController = new userController();
-
-$account = $_userController->getAccount($_account);
-if(is_object($account) or is_array($account)){
-    $_result=$_userController->getValidateAccount($_account);
-    if(is_array($_result)){
-        echo "Error, It is neccesary to make payouts to your bank account, the next fields <br>";
-        print_r($_result);
+    $_userController = new userController();
+    $account = $_userController->getAccount($_account);
+    if(is_object($account) or is_array($account)){
+        $_result=$_userController->getValidateAccount($_account);
+        if(is_array($_result)){
+            echo "Error, It is neccesary to make payouts to your bank account, the next fields <br>";
+            print_r($_result);
+        }else{
+            echo $_result;
+        }
+        
     }else{
-        echo $_result;
+        echo "Error, the account was not found";
     }
-    
 }else{
     echo "Error, the account was not found";
 }
